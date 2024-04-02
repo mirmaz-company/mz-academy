3266beaa05fd38c192fe768ede122f38a5fb54d5
Mohamed Gohar <mohamedgohar365@gmail.com>
2024-04-02 04:08:31 +0200

    fix issue config/app and change php V in fly.toml

--- config/app.php
+++ config/app.php
@@ -183,8 +183,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
     ],
+
 
     /*
     |--------------------------------------------------------------------------
@@ -198,7 +198,6 @@ return [
     */
 
     'aliases' => [
-
         'App' => Illuminate\Support\Facades\App::class,
         'Arr' => Illuminate\Support\Arr::class,
         'Artisan' => Illuminate\Support\Facades\Artisan::class,
@@ -244,8 +243,7 @@ return [
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
         'Vimeo' => Vimeo\Laravel\Facades\Vimeo::class,
         'Youtube' => Dawson\Youtube\Facades\Youtube::class,
-        'PDF' => Meneses\LaravelMpdf\Facades\LaravelMpdf::class
-
+        'PDF' => Mccarlosen\LaravelMpdf\Facades\LaravelMpdf::class,
     ],
 
 ];
