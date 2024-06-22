8a3a90c9fb63681dddd4e2cc64b15d36acc5e1cb
Mohamed Gohar <mohamedgohar365@gmail.com>
2024-06-22 21:00:11 +0300

    add project in dir "laravel-app"

---
+++ laravel-app/vendor/fly-apps/dockerfile-laravel/app/Services/Scanner.php
@@ -0,0 +1,52 @@
+<?php
+namespace App\Services;
+
+use Illuminate\Support\Facades\Process;
+
+/**
+* This service scans configuration values for a Laravel project
+*/
+class Scanner
+{
+    /**
+    * Scan for the Laravel version
+    */
+    public function laravelVersion()
+    {
+        // From artisan command
+        $run = Process::run( 'php artisan --version' );
+        $version = $run->output();
+        $version = explode('Laravel Framework', $version);
+        if( count($version) >1 ){
+            return trim($version[1]);
+        }
+
+        // Default Latest Version
+        return  "11.0.0";
+    }
+
+
+    /**
+     * Scans for templates to generate
+     * 
+     * @return array
+     *      key is template name, and the value is the output file name.
+     */
+    public function templates()
+    {
+        // Define the list of templates to render.
+        // The key is the template name, and the value is the output file name.
+        $templates = [
+            'dockerfile' => 'Dockerfile',
+        ];
+
+        // Scan for fly.io mark
+        if ( file_exists('fly.toml') ) {
+            $templates[ 'fly.dockerignore' ] = '.dockerignore';
+            $templates[ 'fly.entrypoint']    = '.fly/entrypoint.sh';
+            $templates[ 'fly.scripts.caches'] = '.fly/scripts/caches.sh';
+        }
+
+        return $templates;
+    }
+}
